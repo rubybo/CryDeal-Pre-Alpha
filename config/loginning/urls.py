f2c6917a39b45b23663cd4da553abe05d771e223
rubybo <Kapturalex4@gmail.com>
2023-10-17 23:27:53 +0300

    Переработка некоторых компонентов

--- config/loginning/urls.py
+++ config/loginning/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import LoginUser, LogoutUser
 
-app_name = 'loginning'
+
 
 urlpatterns = [
-    path('user-login/', LoginUser.as_view(), name='user-login'),
-    path('user-logout/', LogoutUser.as_view(), name='user-logout'),
+
 
 ]
--- config/loginning/views.py
+++ config/loginning/views.py
@@ -3,15 +3,15 @@ from django.contrib.auth.views import LoginView, LogoutView, reverse_lazy
 from .forms import UserLoginForm
 
 
-# Cbv LoginView
-class LoginUser(LoginView):
-    form_class = UserLoginForm
-    template_name = 'loginning/login.html'
-
-
-# Cbv LogoutView
-class LogoutUser(LogoutView):
-    template_name = 'core/base.html'
-
-    def get_success_url(self):
-        return reverse_lazy('loginning:user-login')
+# # Cbv LoginView
+# class LoginUser(LoginView):
+#     form_class = UserLoginForm
+#     template_name = 'loginning/login.html'
+#
+#
+# # Cbv LogoutView
+# class LogoutUser(LogoutView):
+#     template_name = 'core/base.html'
+#
+#     def get_success_url(self):
+#         return reverse_lazy('loginning:user-login')
